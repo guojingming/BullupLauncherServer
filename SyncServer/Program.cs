28417df82649d97a8e3e518b5af6497422b835b3
JM.Guo <1043840980@qq.com>
2017-12-29 17:18:11 +0800

    latest

---
+++ SyncServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCPLib;
+
+namespace SyncServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //指定IP和端口号及最大监听数目的方式
+            TCPLib.TCPServer s1 = new TCPServer("127.0.0.1", 6001, 10);
+            //指定端口号及最大监听数目的方式
+            TCPLib.TCPServer s2 = new TCPServer(6001, 10);
+
+            //执行Start方法
+            s1.Start();
+        }
+    }
+}
